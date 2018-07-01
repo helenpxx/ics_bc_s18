7fddde759acc88340bc5b9cada060ec4898fe595
helenpxx <helenpeng2000@gmail.com>
2018-07-01 09:22:35 -0700

    Complete full_name_greeting exercise

---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts "What is your first name?"
+first_name = gets
+puts "What is your middle name?"
+middle_name = gets
+puts "What is your last name?"
+last_name = gets
+puts "Nice to meet you, " + first_name + " " + middle_name + " " + last_name + "."
