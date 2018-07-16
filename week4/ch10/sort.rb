ac06fc914965fa33b6b264d11b4eb004365619fb
helenpxx <helenpeng2000@gmail.com>
2018-07-16 09:21:57 -0700

    Complete sort exercise

--- week4/ch10/sort.rb
+++ week4/ch10/sort.rb
@@ -4,6 +4,26 @@ end
 
 def recursive_sort(unsorted_array, sorted_array)
   # Your fabulous code goes here.
+  if unsorted_array.length == 0
+    sorted_array
+  else
+  min_index = min_ind unsorted_array
+  min_elem = unsorted_array.delete_at min_index
+  sorted_array.push min_elem
+  recursive_sort unsorted_array, sorted_array
+  end
 end
 
-sort [__, __, ...]
+def min_ind(array)
+  min_ind = 0
+  ind = 1
+  while ind < array.length
+    if array[ind] < array [min_ind]
+      min_ind = ind
+    end
+    ind += 1
+  end
+  min_ind
+end
+
+puts sort ['d','c','b']
