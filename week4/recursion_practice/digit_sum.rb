838406a25496c97767effc8eee8c13ec9ac8926e
helenpxx <helenpeng2000@gmail.com>
2018-07-18 07:32:22 -0700

    Complete digit_sum exercise

--- week4/recursion_practice/digit_sum.rb
+++ week4/recursion_practice/digit_sum.rb
@@ -1,9 +1,9 @@
-def digit_sum(num, ___)
-  if ______________________
-    ______________________
+def digit_sum(num, len)
+  if len == 1
+    num
   else
-    ______________________
+    num%10 + digit_sum(num/10, len-1)
   end
 end
 
-puts digit_sum __, __ 
+puts digit_sum 819, 3
