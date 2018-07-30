85c344c880a97fc0640b4590a2bc99a247320d89
helenpxx <helenpeng2000@gmail.com>
2018-07-30 06:00:25 -0700

    Complete die exercise

--- week6/ch13/die.rb
+++ week6/ch13/die.rb
@@ -12,4 +12,15 @@ class Die
   end
 
   # Define a cheat method here
+  def cheat
+    puts "Set the number you want: "
+    num = gets.chomp.to_i
+    if num > 0 && num < 7
+      @number_showing = num
+    end
+  end
 end
+
+die = Die.new
+die.cheat
+puts "The number facing is: #{die.showing}"
